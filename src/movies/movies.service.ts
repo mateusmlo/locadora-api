10f4fa9bc1555fbf862ebb8fd59270a839c16b1c
Mateus M <mateus.mlo95@gmail.com>
2020-10-16 15:35:29 -0300

    Implement remaining CRUD ops
    It is now possible to create, read (all and by ID), update and delete (by ID) movies.

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateMovieDTO } from './dto/create-movie.dto';
+import { MovieDTO } from './dto/movie.dto';
 import { MovieRepository } from './movie.repository';
 
 @Injectable()
@@ -10,7 +10,52 @@ export class MoviesService {
     private movieRepository: MovieRepository,
   ) {}
 
-  async createMovie(createMovieDto: CreateMovieDTO) {
-    return this.movieRepository.createMovie(createMovieDto);
+  async getMovies() {
+    return this.movieRepository.getMovies();
+  }
+
+  async getMovieByID(id: number) {
+    const movie = await this.movieRepository.findOne(id);
+
+    if (!movie) throw new NotFoundException(`Movie with ID ${id} not found.`);
+
+    return movie;
+  }
+
+  async createMovie(movieDto: MovieDTO) {
+    return this.movieRepository.createMovie(movieDto);
+  }
+
+  async updateMovie(id: number, movieDto: MovieDTO) {
+    const {
+      title,
+      synopsis,
+      gender,
+      release_date,
+      language,
+      subbed,
+      director,
+      IMDB,
+      quantity,
+    } = movieDto;
+
+    const movie = await this.getMovieByID(id);
+
+    movie.title = title;
+    movie.synopsis = synopsis;
+    movie.gender = gender;
+    movie.release_date = release_date;
+    movie.language = language;
+    movie.subbed = subbed;
+    movie.director = director;
+    movie.IMDB = IMDB;
+    movie.quantity = quantity;
+
+    await movie.save();
+    return movie;
+  }
+
+  async deleteMovie(id: number) {
+    return await this.movieRepository.delete(id);
   }
 }
